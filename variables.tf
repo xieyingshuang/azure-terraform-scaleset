e9dc536530ffc1d1bb55c1eaf52b57b14e3e2df5
xieyingshuang <xieyingshuang@outlook.com>
2018-04-11 10:59:18 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "location" {
 
 variable "resource_group_name" {
   description = "The name of the resource group in which the resources are created"
-  default     = "myterraform"
+  default     = "myterraform2"
 }
 
 variable "application_port" {
